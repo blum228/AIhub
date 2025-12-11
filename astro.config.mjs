3013c29ee901c948372afc2664d72e082a5a2d68
blum <jack2jack2223@gmail.com>
2025-12-11 05:35:39 +0300

    D2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Для GitHub Pages с подпапкой (замени на свой репозиторий)
+  site: 'https://blum228.github.io/ids-astro',
+  base: '/ids-astro',
+  output: 'static',
+});
